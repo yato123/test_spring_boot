ee0c237c81dec7d5c0d305bb8fbcf534e80448a1
陈子龙 <635317490@qq.com>
2019-08-19 20:19:44 +0800

    spring

---
+++ src/main/java/com/example/testspringboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.testspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "Helllo Spring Boot";
+    }
+}
